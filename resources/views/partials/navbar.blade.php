2495e444f07db0d82eb114f18fd4c86bf8618eb4
ferdylistant <ferdylucker@.com>
2025-12-16 14:59:04 +0700

    init

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -5,9 +5,15 @@
             <img src="{{ asset('img/logo.png') }}" height="40" alt="Kiniko Art Management">
         </a>
 
-        <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navMain">
-            <span class="navbar-toggler-icon"></span>
+        <button class="navbar-toggler kiniko-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navMain"
+            aria-controls="navMain" aria-expanded="false" aria-label="Toggle navigation">
+
+            <span class="toggler-line"></span>
+            <span class="toggler-line"></span>
+            <span class="toggler-line"></span>
+
         </button>
+
 
         <div class="collapse navbar-collapse" id="navMain">
             <ul class="navbar-nav ms-auto gap-3 text-uppercase small">
